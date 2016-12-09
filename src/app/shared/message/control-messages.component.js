0c03f0272779743509988b2a343cced0c0d7ec4a
khiemnvd <khiemnvd.dev@gmail.com>
2016-12-09 16:56:41 +0700

    update

--- src/app/shared/message/control-messages.component.js
+++ src/app/shared/message/control-messages.component.js
@@ -8,9 +8,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require('@angular/core');
-var forms_1 = require('@angular/forms');
-var validation_service_1 = require('./validation.service');
+var core_1 = require("@angular/core");
+var forms_1 = require("@angular/forms");
+var validation_service_1 = require("./validation.service");
 var ControlMessagesComponent = (function () {
     function ControlMessagesComponent() {
     }
@@ -26,18 +26,18 @@ var ControlMessagesComponent = (function () {
         configurable: true
     });
     ControlMessagesComponent.prototype.ngOnInit = function () { };
-    __decorate([
-        core_1.Input(), 
-        __metadata('design:type', forms_1.FormControl)
-    ], ControlMessagesComponent.prototype, "control", void 0);
-    ControlMessagesComponent = __decorate([
-        core_1.Component({
-            selector: 'control-messages',
-            template: "<div *ngIf=\"errorMessage !== null\">{{errorMessage}}</div>"
-        }), 
-        __metadata('design:paramtypes', [])
-    ], ControlMessagesComponent);
     return ControlMessagesComponent;
 }());
+__decorate([
+    core_1.Input(),
+    __metadata("design:type", forms_1.FormControl)
+], ControlMessagesComponent.prototype, "control", void 0);
+ControlMessagesComponent = __decorate([
+    core_1.Component({
+        selector: 'control-messages',
+        template: "<div *ngIf=\"errorMessage !== null\">{{errorMessage}}</div>"
+    }),
+    __metadata("design:paramtypes", [])
+], ControlMessagesComponent);
 exports.ControlMessagesComponent = ControlMessagesComponent;
 //# sourceMappingURL=control-messages.component.js.map
