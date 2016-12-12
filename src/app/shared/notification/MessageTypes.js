60ee0597598c7f3af325ee1c1b248e46d103f96d
khiemnvd <khiemnvd.dev@gmail.com>
2016-12-12 16:27:47 +0700

    Add
    list of message type's values

---
+++ src/app/shared/notification/MessageTypes.js
@@ -0,0 +1,9 @@
+"use strict";
+var MessageTypes;
+(function (MessageTypes) {
+    MessageTypes[MessageTypes["SUCCESS"] = 'SUCCESS'] = "SUCCESS";
+    MessageTypes[MessageTypes["ERROR"] = 'ERROR'] = "ERROR";
+    MessageTypes[MessageTypes["WARNING"] = 'WARNING'] = "WARNING";
+    MessageTypes[MessageTypes["INFO"] = 'INFO'] = "INFO";
+})(MessageTypes = exports.MessageTypes || (exports.MessageTypes = {}));
+//# sourceMappingURL=MessageTypes.js.map
---
+++ src/app/shared/notification/MessageTypes.ts
@@ -0,0 +1,6 @@
+export enum MessageTypes {
+     SUCCESS = <any>'SUCCESS',
+     ERROR = <any>'ERROR',
+     WARNING = <any>'WARNING',
+     INFO = <any>'INFO'
+}
